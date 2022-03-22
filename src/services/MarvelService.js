4d0e7a5109745ce8196f57b8eb5c228277c5fc97
Dima Bart <3088742@mail.ru>
2022-03-22 23:26:54 +0300

    add char info

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -18,17 +18,18 @@ class MarvelService {
 
 	getCharacter = async (id) => {
 		const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-		console.log(this._transformCharacter(res.data.results[0]).thumbnail)
 		return this._transformCharacter(res.data.results[0]);
 	}
 
 	_transformCharacter = (char) => {
 		return {
+			id: char.id,
 			name: char.name,
 			description: char.description,
 			thumbnail:char.thumbnail.path + '.' + char.thumbnail.extension,
 			homepage: char.urls[0].url,
-			wiki: char.urls[1].url
+			wiki: char.urls[1].url,
+			comics: char.comics.items
 		}
 	}
 }
