eed3cf1ca0a999b19a90846e57391cf126b4a1e6
Dima Bart <3088742@mail.ru>
2022-05-11 00:08:10 +0300

    add helmet

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {Helmet} from "react-helmet"
 
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -18,26 +19,33 @@ const MainPage = () => {
     }
 
     return (
-        <>
+      <>
+        <Helmet>
+          <meta 
+			 	name="description" 
+			 	content="Marvel information portal" 
+				 />
+          <title>Marvel information portal</title>
+        </Helmet>
+        <ErrorBoundary>
+          <RandomChar />
+        </ErrorBoundary>
+        <div className="char__content">
+          <ErrorBoundary>
+            <CharList onCharSelected={onCharSelected} />
+          </ErrorBoundary>
+          <div>
             <ErrorBoundary>
-                <RandomChar/>
+              <CharInfo charId={selectedChar} />
             </ErrorBoundary>
-            <div className="char__content">
-                <ErrorBoundary>
-                    <CharList onCharSelected={onCharSelected}/>
-                </ErrorBoundary>
-                <div>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharSearchForm/>
-                    </ErrorBoundary>
-                </div>
-            </div>
-            <img className="bg-decoration" src={decoration} alt="vision"/>
-        </>
-    )
+            <ErrorBoundary>
+              <CharSearchForm />
+            </ErrorBoundary>
+          </div>
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </>
+    );
 }
 
 export default MainPage;
--- src/components/pages/singleComicLayout/SingleComicLayout.js
+++ src/components/pages/singleComicLayout/SingleComicLayout.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { Helmet } from "react-helmet";
 
 import './singleComicLayout.scss';
 
@@ -7,18 +8,24 @@ const SingleComicLayout = ({data}) => {
     const {title, description, pageCount, thumbnail, language, price} = data;
 
     return (
-        <div className="single-comic">
-            <img src={thumbnail} alt={title} className="single-comic__img"/>
-            <div className="single-comic__info">
-                <h2 className="single-comic__name">{title}</h2>
-                <p className="single-comic__descr">{description}</p>
-                <p className="single-comic__descr">{pageCount}</p>
-                <p className="single-comic__descr">Language: {language}</p>
-                <div className="single-comic__price">{price}</div>
-            </div>
-            <Link to="/comics" className="single-comic__back">Back to all</Link>
+      <div className="single-comic">
+        <Helmet>
+          <meta name="description" content={`${title} comics book`} />
+          <title>{title}</title>
+        </Helmet>
+        <img src={thumbnail} alt={title} className="single-comic__img" />
+        <div className="single-comic__info">
+          <h2 className="single-comic__name">{title}</h2>
+          <p className="single-comic__descr">{description}</p>
+          <p className="single-comic__descr">{pageCount}</p>
+          <p className="single-comic__descr">Language: {language}</p>
+          <div className="single-comic__price">{price}</div>
         </div>
-    )
+        <Link to="/comics" className="single-comic__back">
+          Back to all
+        </Link>
+      </div>
+    );
 }
 
 export default SingleComicLayout;
